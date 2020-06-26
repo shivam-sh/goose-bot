2c0a8cdb960c1b9102e41dfc3a93e605d7b4646f
Shivam Sharma <shivam.sharm@icloud.com>
2020-06-25 21:42:02 -0400

    Connected Waterloo Open API and created database

--- bot.js
+++ bot.js
@@ -1,9 +1,17 @@
 // Keep server online (only needer for repl.it hosting)
 const keep_alive = require('./keep_alive.js')
 
-// Main Program
+// Functions for data storage & managing profiles
+const people = require("./people.js");
+
+// Helps parse .json from webserver
+const fetch = require('node-fetch');
+
+// Discord Bot
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
+
+let prefix = "~";
 
 bot.on("ready", async () => {
   console.log(`[LOGIN] ${bot.user.tag} is online!`);
@@ -21,25 +29,61 @@ bot.on('reconnecting', () => {
 bot.on("message", async msg => {
   if (msg.author.bot) return;
   
-  let msgArray = msg.content.split(" ");
-  let cmd = msgArray[0];
-  let args = msgArray.slice(1);
-  
-  if(cmd.substring(0,1) == prefix) {
+  if(msg.content.substring(0,1) == prefix) {
+    let msgArray = msg.content.split(" ");
+    let cmd = msgArray[0];
+    let args = msgArray.slice(1);
+
     switch(cmd) {
       case `${prefix}hello`:
-        return msg.reply(" Hello!");
+        msg.reply(" Hello!");
         break;
-        
+
+      case '${prefix}verify`':
+        // check if arg[1] is valid userID
+          // if it is generate a verification token and store it in the database along with person's info
+          // email the person their userID if in SYDE
+        break;
+
+      case `${prefix}addUser`:
+        if (args.length != 1) {
+          msg.reply(" Invalid syntax, try ~addUser [userID]");
+        }
+
+        var url = 'https://api.uwaterloo.ca/v2/directory/' + args[0] + '.json?key=' + process.env.API_KEY_V2
+        let settings = { method: "Get" };
+
+        fetch(url, settings)
+            .then(res => res.json())
+            .then((json) => {
+                console.log(json);
+                console.log(json.meta.message);
+                if(json.meta.message == 'Request successful') {
+                  people.addPerson(json.data.user_id, json.data.given_name, json.data.last_name, json.data.department)
+                }
+            });
+        break;
+
+        case '${prefix}confirm':
+        //people.addPerson(userID, firstName, lastName, department);
+        // check person's userID against their token
+          // if valid it gives them the verified role
+          // if person is syde it goves them the correct role
+        break;
+      
+      case `${prefix}logData`:
+        people.logData();
+        break;
+
       case `${prefix}ping`:
-        return msg.reply(" Pong!");
+        msg.reply(" Pong!");
         break;
         
       default:
-        return msg.reply(" Invalid :(");
+        msg.reply(" Invalid :(");
         break;
     }
   }
 });
 
-client.login(process.env.TOKEN);
+bot.login(process.env.TOKEN);
