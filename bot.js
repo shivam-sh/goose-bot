fedea93eb3cc136c71312d98f789fb2dbbb6fa81
Shivam Sharma <shivam.sharm@icloud.com>
2020-06-27 12:45:23 -0400

    Large refactor for clarity

--- bot.js
+++ bot.js
@@ -2,13 +2,17 @@
 const keep_alive = require('./keep_alive.js')
 
 // Functions for data storage & managing profiles
-const people = require("./people.js")
+const people = require('./people.js')
+
+// Preset variables
+const preset =require('./presets.json')
+
+// Email plugin to send verification codes
+const email = require('emailjs')
 
 // The Discord Bot itself
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-
-let prefix = "~"
 
 bot.on("ready", async () => {
   console.log(`\n\n[LOGIN] ${bot.user.tag} is online!\n\n`)
@@ -28,98 +32,110 @@ bot.on('reconnecting', () => {
 bot.on("message", async msg => {
   if (msg.author.bot) return
   
-  if(msg.content.substring(0,1) == prefix) {
+  if(msg.content.substring(0,1) == preset.prefix) {
     let msgArray = msg.content.split(" ")
     let cmd = msgArray[0]
     let args = msgArray.slice(1)
 
 
-    // Handle various commands
+    // Handle bot commands
     switch(cmd) {
       // Simple greeting
       case `${prefix}hello`:
         msg.channel.send(" Hello!")
         break
 
-      case `${prefix}init`:
-        people.initDatabase(msg.guild.id)
-        break
 
       // Verify new users through e-mail
-      case `${prefix}verify`:
+      case `${preset.prefix}verify`:
         console.log(`[VERIFY] for: ${args[0]}`)
         if (args.length != 1) {
-          msg.channel.send(`Invalid syntax, try ${prefix}verify [userID]`)
-          console.log(`[VERIFY] Invalid Syntax, Try Again!`)
+          msg.channel.send(`Invalid syntax, try ${preset.prefix}verify [userID]`)
+          console.log(`[\\VERIFY] Invalid Usage, Try Again!`)
           break
         }
         if (people.isLogged(args[0], msg.guild.id)) {
           msg.channel.send("That person is already in the database! If you think this is an error please use @Admin")
-          console.log(`[VERIFY] Already Logged!`)
+          console.log(`[\\VERIFY] Already Logged!`)
           break
         }
 
-        var response = people.addPerson(args[0], msg.guild.id)
+        var guild = msg.guild.id
+        var response = people.lookup(args[0])
 
-        switch(response) {
-          case 2:
+        if (response === 0) {
+          people.loadData(guild)
+
+          response = people.addPerson(args[0], guild)
+
+          if (response === 0) {
+            msg.channel.send(`Great! I added you to my database`)
+            people.assignToken(args[0], guild)
+
+            
+            
+
+
+            people.saveData(guild)
+          }
+          else if (response === 1) {
             msg.channel.send(`Uh Oh... Something went wrong! \nContacting <@&694339748528914472>...`)
             console.log(`[VERIFY -> ERROR] while adding to database :(`)
-            break
-          case 1:
-            msg.channel.send(`Doesn't look like you're in UWs database! This may be due to an invalid UserID, try again. \nIf you think this is a mistake use @Admin!`)
-            console.log(`[VERIFY] Invalid UserID: Try Again!`)
-            break
-          case 0:
-            msg.channel.send(`Great! I added you to my database`)
-            break
-          default:
-            console.log(response)
-            break
+          }
+
+        } 
+        else if (response === 1) {
+          msg.channel.send(`Doesn't look like you're in UWs database! This may be due to an incorrect UserID, try again. \nIf you think this is a mistake use @Admin!`)
+          console.log(`[\\VERIFY] Invalid UserID: Try Again!`)
         }
-       
+
           // email the person their userID if in SYDE
         break
 
-      case `${prefix}addUser`:
+
+      // Confirm the user's identity with their token
+      case `${preset.prefix}confirm`:
+        //people.addPerson(userID, firstName, lastName, department);
+        // check person's userID against their token
+        // if valid it gives them the verified role
+        // if person is syde it gives them the correct role
+        break
+
+      // Manually verify a user
+      case `${preset.prefix}verifyUser`:
         if (!msg.member.hasPermission('ADMINISTRATOR')) {
           msg.reply(`You need Admin privileges to use that command!`)
           break
         }
-        
         if (args.length != 1) {
-          msg.channel.send(`Invalid syntax, try ${prefix}addUser [userID]`)
+          msg.channel.send(`Invalid syntax, try ${preset.prefix}addUser [userID]`)
           break
         }
-
         if (people.isLogged[args[1]]) {
           msg.channel.send("That person is already in the database! No use in adding them again. If you think this is an error please contact @Admin")
           break
         }
 
         break
-
-      case `${prefix}confirm`:
-        //people.addPerson(userID, firstName, lastName, department);
-        // check person's userID against their token
-          // if valid it gives them the verified role
-          // if person is syde it goves them the correct role
-        break
       
-      case `${prefix}logData`:
-        people.logData()
+      // Log people
+      case `${preset.prefix}logData`:
+        people.logData(msg.guild.id)
         break
 
-      case `${prefix}ping`:
-        msg.reply(" Pong!")
+
+
+      // Random Commands
+      case `${preset.prefix}honk`:
+        msg.channel.send(" HONK")
         break
 
-      case `${prefix}help`:
+      case `${preset.prefix}help`:
         msg.channel.send(" Still working on that! Check in later :)")
         break
         
       default:
-        msg.channel.send("Sorry, I don't know that command yet :(")
+        msg.channel.send("Sorry, I don't know that command yet :(\nTry `~help` to see what I can do!")
         break
     }
   }
--- people.js
+++ people.js
@@ -11,8 +11,20 @@ var rawData
 var people
 var stats
 
+var fName
+var lName
+var dept
+var email
+var token
+
+
 module.exports = {
 
+    fName,
+    lName,
+    dept,
+    email,
+    token,
     logData: function(guildID) {
         this.loadData(guildID)
 
@@ -29,96 +41,102 @@ module.exports = {
         return false
     },
 
-    addPerson: function(userID, guildID) {
+    lookup: function(userID) {
+        console.log(` - looking up ${userID} in UW database`)
+
         var url = 'https://api.uwaterloo.ca/v2/directory/' + userID + '.json?key=' + process.env.API_KEY_V2
 
-        console.log(url)
         fetch(url, { method: `Get` })
           .then(res => res.json())
           .then((json) => {
               console.log(` - requesting JSON for ${userID}`)
               if(json.meta.message === 'Request successful' ) {
-                try {
-                    console.log(` - received JSON for ${userID}`)
-                    this.loadData(guildID)
 
-                    people[userID] = {
-                        "fName": json.data.given_name,
-                        "lName": json.data.last_name,
-                        "dept": json.data.department,
-                        "eMail": json.data.email_addresses[0],
-                        "verified": false,
-                        "Token": `notGenerated`
-                    }
+                    fName = json.data.given_name
+                    lName = json.data.last_name
+                    dept = json.data.department
+                    eMail = json.data.email_addresses[0]
+                    verified = false
+                    token = `notGenerated`
 
-                    console.log(` - added  ${userID} to ${guildID}`)
+                console.log(` - fetched info for ${userID}`)       
 
-                    stats.numPeople++
-                    if (json.data.department === `ENG/Systems Design`) {
-                        stats.numInSYDE++ 
-                    } else {
-                        stats.numOutSYDE++ 
-                    }           
-
-                    this.saveData(guildID)
-                } catch (error) {
-                    console.log(error)
-                    return 2
-                }
+                return 0
               } else {
-                console.log(` - ${userID} is not in database`)
+                console.log(` - ${userID} doesn't return valid JSON`)   
                 return 1
               }
           })
-        return 0
+
+        return 2
     },
 
-    assignToken: function(userID, guildID) {
+    addPerson: function(userID, guildID) {
         try {
-            this.loadData(guildID)
-        
-            var token = crypto.randomBytes(Math.ceil(10/2)).toString('hex').slice(0,10)
-            people[userID].verifToken = token
-            
-            console.log(` - assigned token to ${userID}`)
+            people[userID] = {
+                "fName": json.data.given_name,
+                "lName": json.data.last_name,
+                "dept": json.data.department,
+                "eMail": json.data.email_addresses[0],
+                "verified": false,
+                "Token": `notGenerated`
+            }
 
-            this.saveData()
-            return token
-        } catch(err) {
-            console.log(err)
-            return false
+            console.log(` - added  ${userID} to ${guildID}`)
+
+            stats.numPeople++
+            if (json.data.department === `ENG/Systems Design`) {
+                stats.numInSYDE++ 
+            } else {
+                stats.numOutSYDE++ 
+            }           
+
+            return 0
+        } catch {
+            return 1
         }
     },
-    initDatabase: function(guildID) {
-        console.log(`[INIT] ID: ${guildID}`)
-        this.loadData(guildID)
 
-        if (!stats.initialized) {
-            stats.initialized = false
-            fs.writeFileSync(`.data/people-${guildID}.json`, `{}`)
-            fs.writeFileSync(`.data/stats-${guildID}.json`, `{"numPeople": 0, "numVerified": 0, "numInSYDE": 0, "numOutSYDE": 0, "initialized": true}`)
-            console.log(`[INIT] Initialized ID: ${guildID}`)
-            return
-        }
+    assignToken: function(userID) {
 
-        console.log(`[INIT] ${guildID} is already in database`)
+        var token = crypto.randomBytes(Math.ceil(10/2)).toString('hex').slice(0,10)
+        people[userID].verifToken = token
+            
+        console.log(` - assigned token to ${userID} for guild ${guildID}`)
+
     },
 
     loadData: function(guildID) {
-        rawData = fs.readFileSync(`.data/people-${guildID}.json`)
-        people = JSON.parse(rawData)
+        console.log(`\n[LOAD DATA] ${guildID}`)
+        try {
+            rawData = fs.readFileSync(`.data/people-${guildID}.json`)
+            people = JSON.parse(rawData)
 
-        rawData = fs.readFileSync(`.data/stats-${guildID}.json`)
-        stats = JSON.parse(rawData)
-        console.log(`[LOAD DATA] ${guildID}`)
+            rawData = fs.readFileSync(`.data/stats-${guildID}.json`)
+            stats = JSON.parse(rawData)
+            console.log(`[\\LOAD DATA] Loaded ${guildID}`)
+        }
+        catch {
+            fs.writeFileSync(`.data/people-${guildID}.json`, `{}`)
+            fs.writeFileSync(`.data/stats-${guildID}.json`, `{"numPeople": 0, "numVerified": 0, "numInSYDE": 0, "numOutSYDE": 0}`)
+
+            rawData = fs.readFileSync(`.data/people-${guildID}.json`)
+            people = JSON.parse(rawData)
+
+            rawData = fs.readFileSync(`.data/stats-${guildID}.json`)
+            stats = JSON.parse(rawData)
+
+            console.log(`[\\LOAD DATA] New Guild! Initialized ${guildID}`)
+        }
     },
 
     saveData: function(guildID) {
+        console.log(`[SAVE DATA] ${guildID}`)
         rawData = JSON.stringify(people, null, 4)
         fs.writeFileSync(`.data/people-${guildID}.json`, rawData)
 
         rawData = JSON.stringify(stats, null, 4)
         fs.writeFileSync(`.data/stats-${guildID}.json`, rawData)
-        console.log(`[SAVE DATA] ${guildID}`)
-    },
+        console.log(`[\\SAVE DATA] Saved ${guildID}\n`)
+    }
 }
