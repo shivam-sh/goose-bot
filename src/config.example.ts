f4f694d375494dfd11f6b69191d1781372426173
Shivam Sharma <shivam.sharm@icloud.com>
2021-09-09 22:01:53 -0400

    Rewrite to support the latest discord.js & API 🚀
    Slash commands are now starting to be supported :)

---
+++ src/config.example.ts
@@ -0,0 +1,17 @@
+export const bot = {
+    prefix: "-",
+    discordToken: "DISCORD_TOKEN"
+}
+
+export const email = {
+    address: "EMAIL",
+    password: "EMAIL_PASS",
+    host: "EMAIL_HOST",
+    port: "EMAIL_PORT"
+}
+
+export const mongo = {
+    username: "MONGO_USER",
+    password: "MONGO_PASS",
+    clusterURI: "MONGO_CLUSTER_URI"
+}
