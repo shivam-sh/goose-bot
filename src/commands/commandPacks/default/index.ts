e16e9c4a4d0929a7fdadc5fa123fb724b9bc58ae
Shivam Sharma <shivam.sharm@icloud.com>
2021-09-10 16:03:44 -0400

    Add types for additional builders

--- src/commands/commandPacks/default/index.ts
+++ src/commands/commandPacks/default/index.ts
@@ -6,7 +6,6 @@
  */
 
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
 import { Command, CommandPack } from '../../commandPackType';
 
 module.exports = class Default implements CommandPack {
